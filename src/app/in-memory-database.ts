aee86ee921258620d85f0b3001f9e493ed02d68e
ABC da contrucao <digital.abcdaconstrucao@gmail.com>
2018-11-22 13:05:09 -0200

    intercepting http reqyhest api in-memory

---
+++ src/app/in-memory-database.ts
@@ -0,0 +1,17 @@
+import {InMemoryDbService} from 'angular-in-memory-web-api';
+
+import { Category } from './pages/categories/shared/category.model';
+
+export class InMemoryDatabase implements InMemoryDbService {
+  createDb() {
+    const categories: Category[] = [
+      { id: 1, name: 'Moradia', description: 'Pagamento de Contas da Casa' },
+      { id: 2, name: 'Saúde', description: 'Plano de Saúde e Remédios' },
+      { id: 3, name: 'Lazer', description: 'Cinema, parques, praia, etc' },
+      { id: 4, name: 'Salário', description: 'Recebimento de salário' },
+      { id: 5, name: 'Freelas', description: 'Trabalhos com freelancer' }
+    ];
+
+    return { categories };
+  }
+}
