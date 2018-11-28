d3869471b944d66d1627cc73234c8e98a45a597b
ABC da contrucao <digital.abcdaconstrucao@gmail.com>
2018-11-28 20:31:59 -0200

    ordenar listagem

--- src/app/pages/entries/entry-list/entry-list.component.ts
+++ src/app/pages/entries/entry-list/entry-list.component.ts
@@ -17,7 +17,8 @@ export class EntryListComponent implements OnInit {
 
   ngOnInit() {
     this.entryService.getAll().subscribe(
-    entries => this.entries = entries, error => alert('Erro ao carregar a lista')
+    entries => this.entries = entries.sort((a,b) => b.id - a.id),
+        error => alert('Erro ao carregar a lista')
     );
   }
 
