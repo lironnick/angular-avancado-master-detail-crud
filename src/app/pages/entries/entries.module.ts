22d14130d70026557adc6de056363ed627f2e0bd
ABC da contrucao <digital.abcdaconstrucao@gmail.com>
2018-11-29 20:37:10 -0200

    estruração do projeto

--- src/app/pages/entries/entries.module.ts
+++ src/app/pages/entries/entries.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { ReactiveFormsModule } from '@angular/forms';
+
+import { SharedModule} from '../../shared/shared.module';
+
 
 import { EntriesRoutingModule } from './entries-routing.module';
 import { EntryListComponent } from './entry-list/entry-list.component';
@@ -14,9 +15,8 @@ import {IMaskModule} from 'angular-imask';
 @NgModule({
   declarations: [EntryListComponent, EntryFormComponent],
   imports: [
-    CommonModule,
+    SharedModule,
     EntriesRoutingModule,
-    ReactiveFormsModule,
     CalendarModule,
     IMaskModule
   ]
