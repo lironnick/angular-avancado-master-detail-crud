7aeb163b6767624a9cd525308affe7f0bf3ca2a0
ABC da contrucao <digital.abcdaconstrucao@gmail.com>
2018-12-03 20:38:35 -0200

    modificacao do update e create service.ts

--- src/app/pages/entries/shared/entry.service.ts
+++ src/app/pages/entries/shared/entry.service.ts
@@ -1,6 +1,6 @@
 import { Injectable, Injector } from '@angular/core';
 import {Observable} from 'rxjs';
-import {flatMap} from 'rxjs/operators';
+import {catchError, flatMap} from 'rxjs/operators';
 import {BaseResourceService} from '../../../shared/services/base-resource.service';
 
 import { Entry } from './entry.model';
@@ -12,7 +12,6 @@ import {element} from 'protractor';
 })
 export class EntryService extends BaseResourceService<Entry>{
 
-
   constructor(
     protected injector: Injector,
     private  categoryService: CategoryService
@@ -20,26 +19,21 @@ export class EntryService extends BaseResourceService<Entry>{
     super('api/entries', injector, Entry.formJson)
   }
 
-
   create(entry: Entry): Observable<Entry> {
-    return this.categoryService.getById(entry.categoryId).pipe(
-      flatMap(category => {
-        entry.category = category;
-        return super.create(entry)
-      })
-    );
-
+    return this.setCategoryAndSendToServer(entry, super.create.bind(this));
   }
 
   update(entry: Entry): Observable<Entry> {
-    return this.categoryService.getById(entry.categoryId).pipe(
-      flatMap(categoy => {
-        entry.category = categoy;
-        return super.update(entry);
-      })
-    );
-
+    return this.setCategoryAndSendToServer(entry, super.update.bind(this));
   }
 
+  private setCategoryAndSendToServer(entry: Entry, sendFn: any): Observable<Entry> {
+    return this.categoryService.getById(entry.categoryId).pipe(
+      flatMap(category => {
+        entry.category = category;
+        return sendFn(entry)
+      }),catchError(this.handleError)
+    );
+  }
 
 }
